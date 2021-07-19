e59eba7e8a79160f3393e6e0b8ae943ee456b0c4
Mir Hussain <mir.hm110@gmail.com>
2021-07-19 13:50:25 +0600

    added any data type

--- Basic Data Types/script.js
+++ Basic Data Types/script.js
@@ -1,7 +1,14 @@
 //* The most common primitive data type in TypeScript is String, Number and Boolean.
-//* Other data types are Array, Object, Function, Enum, Interface, null and undefined.
+//* Other data types are Any, Array, Object, Function, Enum, Interface, null and undefined.
 var myName = "Mir";
 //* By doing this, the typescript will infer the data type
 //* and it will set the data type of myName to string ( myName: string )
 //* so know i wont be able to assign any other data type. For example myName = 12 
 var age;
+//* If I declare a variable without assigning any data 
+//* typescript wont be able to infer the data type,
+//* hence it will set the data type to any.
+age = 68;
+age = "banana";
+//* By setting a data type to any, the typescript will accept any data type.
+//* This spoils the main purpose of using typescript.
--- Basic Data Types/script.ts
+++ Basic Data Types/script.ts
@@ -1,5 +1,5 @@
 //* The most common primitive data type in TypeScript is String, Number and Boolean.
-//* Other data types are Array, Object, Function, Enum, Interface, null and undefined.
+//* Other data types are Any, Array, Object, Function, Enum, Interface, null and undefined.
 
 let myName = "Mir";
 
@@ -8,3 +8,14 @@ let myName = "Mir";
 //* so know i wont be able to assign any other data type. For example myName = 12 
 
 let age;
+
+//* If I declare a variable without assigning any data 
+//* typescript wont be able to infer the data type,
+//* hence it will set the data type to any.
+
+age = 68
+
+age = "banana";
+
+//* By setting a data type to any, the typescript will accept any data type.
+//* This spoils the main purpose of using typescript.
