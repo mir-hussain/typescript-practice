f1fca417ad675781030852bcc7cce1e328d575b9
Mir Hussain <mir.hm110@gmail.com>
2021-07-19 13:43:47 +0600

    added data infer

--- Basic Data Types/script.js
+++ Basic Data Types/script.js
@@ -1,2 +1,7 @@
 //* The most common primitive data type in TypeScript is String, Number and Boolean.
 //* Other data types are Array, Object, Function, Enum, Interface, null and undefined.
+var myName = "Mir";
+//* By doing this, the typescript will infer the data type
+//* and it will set the data type of myName to string ( myName: string )
+//* so know i wont be able to assign any other data type. For example myName = 12 
+var age;
