cfbb288f63a830425e046009bf495419ec36ccc2
Mir Hussain <mir.hm110@gmail.com>
2021-07-19 13:30:31 +0600

    added the header

--- Basic Data Types/script.ts
+++ Basic Data Types/script.ts
@@ -1,1 +1,3 @@
-console.log("mir");
+//* The most common primitive data type in TypeScript is String, Number and Boolean.
+//* Other data types are Array, Object, Function, Enum, Interface, null and undefined.
+
